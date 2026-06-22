1bf068a1467d6cddd894a74d13d8503d2846079f
jruttan1 <jruttan3@uwo.ca>
2026-06-22 18:14:29 -0400

    bug fixes

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,11 +1,5 @@
 import { createBrowserClient } from "@supabase/ssr"
-import { createDemoClient } from "./demo-client"
-import { isDemoAuthEnabled } from "./demo-mode"
 
 export function createClient() {
-  if (isDemoAuthEnabled()) {
-    return createDemoClient()
-  }
-
   return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
 }
